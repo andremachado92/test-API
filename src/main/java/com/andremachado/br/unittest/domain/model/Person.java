d912e54bfb14b55557a2d6d8c6ef85248b5094ca
Andre do Rosario Cavalcante Machado <andre.machado@ITBAM.ORG.BR>
2021-08-25 15:33:44 -0400

    [Added] Person domain class

---
+++ src/main/java/com/andremachado/br/unittest/domain/model/Person.java
@@ -0,0 +1,24 @@
+package com.andremachado.br.unittest.domain.model;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "TB_PERSON")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 15)
+    private String cpf;
+}
